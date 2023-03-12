334ffbda87d15cabd6681514fbff4c7101c826b0
AndreTNakayama <andrenakayama350@gmail.com>
2023-03-11 21:12:05 -0300

    Projeto brainfuck em C e arquivo .txt contendo Ola, Mundo! em Brainfuck

---
+++ discentes/andre_tomio_nakayama/brainfuck/main.c
@@ -0,0 +1,114 @@
+// https://pt.wikipedia.org/wiki/Brainfuck
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// 5000 células de memória
+#define MAX_MEM 5000
+
+int brainfuck(FILE *file, int quantidade) {
+  // mem controla a posição dentro da memória
+  // num_caracter controla o caracter que será verificado
+  int mem = 0, num_caracter = 0;
+  char memoria[MAX_MEM];
+
+  fseek(file, 0, SEEK_END);
+
+  char *caracter = (char *)malloc(quantidade);
+  
+  // Volta o indicador de posição do fluxo para a posição inicial. Isto é, o começo do arquivo.
+  rewind(file);
+  
+  for (int a = 0; a < quantidade; a++) {
+    // Coloca cada caracter do arquivo de entrada dentro da variável caracter
+    caracter[a] = fgetc(file);
+  }
+
+  while (num_caracter < quantidade) {
+    // leitura de caracter por caracter para realizar a ação
+    char acao = caracter[num_caracter];
+    
+    // Incrementa o mem (acessa a célula de memória seguinte) e insere o valor 0 
+    if (acao == '>' && (++mem) == MAX_MEM){
+      mem = 0;
+    }
+
+    // Acessa a célula de memória anterior
+    else if(acao == '<' && !(mem--)){
+      mem = MAX_MEM-1;
+    } 
+
+    // Imprime na tela o caracter relativo à célula de memória selecionada. Pegar o valor e imprime com base na tabela ASCII
+    else if(acao == '.'){
+      putchar(memoria[mem]);
+    }
+
+    // Salva na célula de memória selecionada o código da próxima tecla a ser pressionada
+    else if(acao == ','){
+      memoria[mem]=getchar();
+    } 
+
+    // Incrementa em um, o valor da célula de memória selecionada
+    else if (acao == '+'){
+      memoria[mem]++;
+    }
+
+    // Decrementa em um, o valor da célula de memória selecionada
+    else if (acao == '-'){
+      memoria[mem]--;
+    }
+
+    // Estrutura de controle
+    else if (acao == '[' && !memoria[mem]) {
+      int controle = 1;
+
+      while (controle) {
+        if (++num_caracter == quantidade) return(0);
+        if (caracter[num_caracter] == '[') controle++;
+        if (caracter[num_caracter] == ']') controle--;
+      }
+    } 
+      
+    // Fim da estrutura
+    else if (acao == ']' && memoria[mem]) {
+      int controle = 1;
+
+      while (controle) {
+        if (--num_caracter == -1) return(0);
+        if (caracter[num_caracter] == ']') controle++;
+        if (caracter[num_caracter] == '[') controle--;
+      }
+    } 
+
+    // Verifica se o caracter pertence aos 8 comandos do brainfuck
+    else if (!(acao == '\t' || acao == '\n' || acao == ' ' || acao == '<' ||
+                 acao == '>' || acao == '[' || acao == ']' || acao == '.' || acao == ',' || acao == '+' || acao == '-')) {
+      printf("Comando não encontrado");
+      return(0);
+    }
+    
+    num_caracter++;
+  }
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  // Leitura do arquivo de entrada
+  FILE *file;
+  file = fopen(argv[1], "r");
+
+  int count = 0;
+  char x;
+
+  // Contador de caracteres dentro do arquivo de entrada para percorrer o arquivo e realizar as devidas ações
+  for (x = getc(file); x != EOF; x = getc(file)) {
+    count = count + 1;
+  }
+
+  if (!file) {
+    printf("Arquivo não foi encontrado\n");
+    return (0);
+  }
+
+  return brainfuck(file, count);
+}
