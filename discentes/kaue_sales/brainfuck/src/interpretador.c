26135ab5d9cd3b0ea81d69cd1223cdb044d708b0
kahbyte <kahbyte@gmail.com>
2023-03-13 17:41:06 -0300

    :sparkles: feat: finished interpreter

--- discentes/kaue_sales/brainfuck/src/interpretador.c
+++ discentes/kaue_sales/brainfuck/src/interpretador.c
@@ -1,32 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define CAPACIDADE 10000
+#define CAPACIDADE 30000
 
-void handle_command(command) {
-    switch(command) {
-        default:
-        printf("sorry, unknown command \n");
-        break;
-    }
-}
-
-int main(void) {
-    char fita[CAPACIDADE] = {0}; //modificar
-    char *ponteiro = fita;
-    char command;
-
+int main() {
     FILE *brainfuck = fopen("brainfuck.txt", "r");
-    if(brainfuck == NULL) {
-        printf("404: File not found.");
+    
+    if (!brainfuck) {
+        printf("404: Arquivo não encontrado\n");
         exit(1);
     }
-
-    while (command != EOF) {
-        command = fgetc(brainfuck);
-        handle_command(command);
-    }
-
+    
+    fseek(brainfuck, 0, SEEK_END); //busca o fim do arquivo
+    long tamArquivo = ftell(brainfuck); //pega a posição atual
+    fseek(brainfuck, 0, SEEK_SET); //volta para a posição inicial do arquivo
+    char *code = malloc(tamArquivo + 1);
+    fread(code, 1, tamArquivo, brainfuck); //insere os conteúdos do brainfuck no vetor code.
     fclose(brainfuck);
+    
+    code[tamArquivo] = '\0'; //no final de code, insere um caractere de controle
+
+    char *fita = calloc(CAPACIDADE, sizeof(char));
+    char *ponteiro = fita;
+
+    for (int i = 0; code[i] != '\0'; i++) {
+        switch (code[i]) {
+            case '>': ponteiro++; break;
+            case '<': ponteiro--; break;
+            case '+': (*ponteiro)++; break;
+            case '-': (*ponteiro)--; break;
+            case '.': putchar(*ponteiro); break;
+            case ',': *ponteiro = getchar(); break;
+                
+            case '[':
+                if (*ponteiro == 0) {
+                    int depth = 1;
+                    while (depth > 0) {
+                        i++;
+                        if (code[i] == '[') {
+                            depth++;
+                        } else if (code[i] == ']') {
+                            depth--;
+                        }
+                    }
+                }
+                break;
+                
+            case ']':
+                if (*ponteiro != 0) {
+                    int depth = 1;
+                    while (depth > 0) {
+                        i--;
+                        if (code[i] == ']') {
+                            depth++;
+                        } else if (code[i] == '[') {
+                            depth--;
+                        }
+                    }
+                }
+                break;
+            default:
+                printf("comando desconhecido \n");
+                break;
+        }
+    }
+    
+    free(fita);
+    free(code);
+
     return 0;
 }
