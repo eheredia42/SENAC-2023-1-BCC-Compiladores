18331fbdec82b24870c5540e432083675ba7c96e
Carlos Gadelha <eduardo_gadelha908@outlook.com>
2023-03-12 22:10:41 -0300

    Implementação do loop

--- discentes/carlos_eduardo_gadelha_silva/brainfuck/src/funcoes.c
+++ discentes/carlos_eduardo_gadelha_silva/brainfuck/src/funcoes.c
@@ -1,35 +1,56 @@
 #include <ctype.h>
-// #include <errno.h>
 #include <stdio.h>
 #include <string.h>
 #define MAX_TAMANHO 256
-#define MAX_MEMORIA 65535
+#define MAX_MEMORIA 30000
 
 int P = 0;
 int memoria[MAX_MEMORIA];
+char mem_repeticao[MAX_MEMORIA];
+int repeticao =0;
+
 
 void verificaCaracter(char letra){
-	if(letra == '>')
-		P++;
-	if(letra == '<')
-		P--;
-	if(letra == '+')
-		memoria[P] += 1;
-	if(letra == '-')
-		memoria[P] -= 1;
-	if(letra == '.')
+
+	if(repeticao == 0)
 	{
-		// putchar(memoria[P] + '0');	
-		putchar(memoria[P]);	
+		if(letra == '>')
+			P++;
+		if(letra == '<')
+			P--;
+		if(letra == '+')
+			memoria[P] += 1;
+		if(letra == '-')
+			memoria[P] -= 1;
+		if(letra == '.')
+			putchar(memoria[P]);	
+		if(letra == ',')
+			memoria[P] = getchar();
+		if(letra == '[')
+			repeticao ++;
+		if(letra == ']'){
+			printf("\nRejeitado! Loop finalizado antes de ser inicializado!");
+		 	exit(0);
+		}
 	}
-	if(letra == ',')
-		memoria[P] = getchar();
-	// if(letra == '['){
-	// 	while(letra != ']'){
-			
-	// 	}
-	// }
+
+	else 
+	{
+		if(letra == ']'){
 		
+			repeticao = 0;
+			
+			while(memoria[P] != 0){
+				for(int i =0; i < MAX_MEMORIA ; i++)
+					verificaCaracter(mem_repeticao[i]);
+			}	
+		}
+			
+		else{
+			mem_repeticao[repeticao] = letra;
+			repeticao ++;
+		}
+	}
 }
 
 int verificaSintaxe(char *palavra){
@@ -40,7 +61,6 @@ int verificaSintaxe(char *palavra){
 
 	while (i < strlen(palavra)) {
 		letra = palavra[i];
-		// printf("%c", letra);
 
 		for(int j = 0; alfabeto[j]!=NULL; j++){
 			if(letra == alfabeto[j]){
@@ -49,7 +69,6 @@ int verificaSintaxe(char *palavra){
 			}
 		}
 
-		// printf("\nACEITA %d\n", aceita);
 		if(aceita == 0)
 			return 1;
 
@@ -65,24 +84,17 @@ void lerArquivo(char *nomeArquivo) {
   FILE *file;
   char texto[MAX_TAMANHO];
   char *palavra;
-  unsigned i = 0;
 
   file = fopen(nomeArquivo, "r");
 
   // #LENDO POR LINHA#
   while (fgets(texto, MAX_TAMANHO, file) != NULL) {
     palavra = strtok(texto, " ");
-    // printf("LINHA: %s\n",texto);
 
     while (palavra != NULL) {
-      // printf("Palavra: %s \n", palavra);
-
-      // if (verificaLetra(palavra) < 1)
-      //   printf(" %s ", palavra);
-
 		 if(verificaSintaxe(palavra) != 0)
 		 {	
-			 printf("\nRejeitado! Erro de sintaxe em %s\n", palavra);
+			 printf("\nRejeitado! Caracter %s não pertence aos comandos da linguagem\n", palavra);
 			 exit(0);
 			 
 		 }
@@ -92,11 +104,5 @@ void lerArquivo(char *nomeArquivo) {
   }
 	
 	printf("\nAceito\n");	
-
-	printf("\nMEMORIA:\n");
-	for(int i = 1; i<6; i++){
-		printf(" [%d] ", memoria[i]);
-	}
-	
   fclose(file);
 }
